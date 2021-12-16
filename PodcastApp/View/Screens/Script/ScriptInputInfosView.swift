bb74c92b4bb7795e7a680ca6b97679d3f1140709
Igor Silva <51680417+igorsilvadev@users.noreply.github.com>
2021-12-16 11:07:13 -0300

    feat: Topic insertion text and toolbar buttons

--- PodcastApp/View/Screens/Script/ScriptInputInfosView.swift
+++ PodcastApp/View/Screens/Script/ScriptInputInfosView.swift
@@ -14,7 +14,7 @@ struct Topics: Identifiable{
 }
 
 var initialTopics = [
-    Topics(nameType: "Tema", description: ""),
+    Topics(nameType: "Tema", description: "Um texto de um amigo meu que pediu para digitar isso aqui e agora estamos vendo no que vai dar #vaicurintiam"),
     Topics(nameType: "Vinheta", description: ""),
     Topics(nameType: "Introdução", description: ""),
     Topics(nameType: "Conteúdo", description: ""),
@@ -23,24 +23,51 @@ var initialTopics = [
 ]
 struct ScriptInputInfosView: View {
     var tipoSelecionado: String
-    @State var descriptionBinding: String = ""
     var body: some View {
+        
+        
+        //TODO: CREATE DELETE TOPIC FUNTION
+        
         NavigationView{
             List(initialTopics){ value in
                 Section{
                     Text("\(value.nameType):")
-                    NavigationLink(destination: ScriptInputSpecificInfoView(topicoSelecionado: value.nameType, teste: descriptionBinding)){
-                        //TextField("", text: $descriptionBinding)
+                    NavigationLink(destination: ScriptInputSpecificInfoView(content: value.description)){
                         Text("\(value.description)")
+                            .frame(height: value.description.isEmpty ? 10 : 100, alignment: .leading)
                     }
                 }
             }//End List
             .navigationTitle("Roteiro: \(tipoSelecionado)")
-        }//End NavigationView
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button {
+                        //TODO: Create action to add topic
+                    } label: {
+                        VStack{
+                            Image(systemName: "plus.circle.fill").foregroundColor(.black)
+                            Text("Adicionar Tópico").foregroundColor(.black)
+                        }.padding(.top)
+                    }
+                }
+                ToolbarItem(placement: .bottomBar) {
+                    Button {
+                        //TODO: Create action to present text
+                    } label: {
+                        VStack{
+                            Image(systemName: "doc.plaintext").foregroundColor(.black)
+                            Text("Visualizar").foregroundColor(.black)
+                        }.padding(.top)
+                    }
+
+                }
+            }
+        }
+        //End NavigationView
     }//End Body
 }
 struct ScriptInputInfosView_Previews: PreviewProvider {
     static var previews: some View {
-        ScriptInputInfosView(tipoSelecionado: "a")
+        ScriptInputInfosView(tipoSelecionado: "Resenha")
     }
 }
