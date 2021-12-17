59564cfca12fa91921fdf64eb91afd16d73b8c7b
Igor Silva <51680417+igorsilvadev@users.noreply.github.com>
2021-12-17 17:15:01 -0300

    feat: Core data CRUD methods

--- PodcastApp/View/Screens/Home/HomeView.swift
+++ PodcastApp/View/Screens/Home/HomeView.swift
@@ -67,7 +67,7 @@ struct HomeView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        HomeView()
     }
 }
 //
