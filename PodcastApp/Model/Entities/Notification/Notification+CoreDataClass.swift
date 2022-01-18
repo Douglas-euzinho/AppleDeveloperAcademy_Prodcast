c05baf0e41d7c28de7a416a4406966bf52339f6a
Igor Silva <51680417+igorsilvadev@users.noreply.github.com>
2022-01-18 16:18:25 -0300

    feat: Core Data Model of Profile and Notification

---
+++ PodcastApp/Model/Entities/Notification/Notification+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Notification+CoreDataClass.swift
+//  PodcastApp
+//
+//  Created by Igor Samoel da Silva on 18/01/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Notification)
+public class Notification: NSManagedObject {
+
+}
