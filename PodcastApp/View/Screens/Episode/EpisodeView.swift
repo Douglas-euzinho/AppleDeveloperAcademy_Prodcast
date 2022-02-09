27bfcc704fc732138c8631120d7272803538f818
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-02-09 15:09:33 -0300

    fix: Changes made in the episodeView

--- PodcastApp/View/Screens/Episode/EpisodeView.swift
+++ PodcastApp/View/Screens/Episode/EpisodeView.swift
@@ -18,107 +18,171 @@ struct EpisodeView: View {
     
     // MARK: - BODY
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-                Text(episodeViewModel.episode?.title ?? "Sem título")
-                    .font(.title)
-                    .padding(.horizontal, 25)
-                    .padding(.bottom, 15)
-                
-                Text("Progresso")
-                    .font(.title2)
-                    .padding(.horizontal, 25)
-                
-                NavigationLink(destination: EmptyView()) {
-                    GroupBox {
-                        VStack {
-                            HStack {
-                                Text("Editar Progresso")
-                                    .font(.subheadline)
-                                Spacer()
-                                Image(systemName: "chevron.forward")
-                                    .font(.subheadline)
-                            }
-                            .foregroundColor(.primary)
-                            
-                            EpisodeProgressView(episode: episodeViewModel.episode ?? Episode())
-                                .offset(x: -65)
-                        }
-                    }
-                }
-                .padding(.horizontal, 25)
-                .padding(.bottom, 25)
-                
-                Text("Teleprompter")
-                    .font(.title2)
-                    .padding(.horizontal, 25)
-                
-                if episodeViewModel.episode?.script != nil {
-                    NavigationLink(destination: ScriptFormatView().environmentObject(episodeViewModel)) {
-                        GroupBox(label: Label("Resenha", systemImage: "text.justify")
-                        ) {
-                            ScrollView(.vertical, showsIndicators: false) {
-                                Text("Olá, aqui é Maria e você está ouvindo ao podcast Vida de Estudante. Nesse episódio iremos falar sobre os tipos existente de Alfabetos, para utilizar em seus mapas mentais e materiais de estudos.")
-                                    .multilineTextAlignment(.center)
-                            }
-                        }
+        ZStack {
+            Color("background-color")
+                .ignoresSafeArea(.all)
+            
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text(episodeViewModel.episode?.title ?? "Sem título")
+                        .font(.system(size: 34))
+                        .fontWeight(.semibold)
                         .padding(.horizontal, 25)
-                        .padding(.bottom, 25)
-                        .buttonStyle(PlainButtonStyle())
-                        .frame(minHeight: 200)
+                        .padding(.bottom, 15)
+                        .offset(y: -20)
+                    
+                    Text("Progresso")
+                        .fontWeight(.semibold)
+                        .modifier(textFieldTitle())
+                    
+                    NavigationLink(destination: EmptyView()) {
+                        GroupBox {
+                            VStack {
+                                HStack {
+                                    Text("Editar Progresso")
+                                        .fontWeight(.semibold)
+                                        .font(.subheadline)
+                                    Spacer()
+                                    Image(systemName: "chevron.forward")
+                                        .modifier(groupBoxChevron())
+                                } //: HSTACK
+                                .foregroundColor(.primary)
+                                
+                                EpisodeProgressView(episode: episodeViewModel.episode ?? Episode())
+                                    .offset(x: -65)
+                            } //: VSTACK
+                            .padding()
+                        } //: GROUP BOX
+                        .groupBoxStyle(groupBoxStroked())
                     }
-                } else {
-                    GroupBox {
-                        VStack {
-                            HStack {
-                                Text("Iniciar Teleprompter")
-                                    .font(.subheadline)
-                                Spacer()
-                                Image(systemName: "chevron.forward")
-                                    .font(.subheadline)
+                    .modifier(textFieldPadding())
+                    
+                    Text("Teleprompter")
+                        .fontWeight(.semibold)
+                        .modifier(textFieldTitle())
+                    
+                    if episodeViewModel.episode?.script != nil {
+                        NavigationLink(destination: ScriptFormatView().environmentObject(episodeViewModel)) {
+                            GroupBox(label: Label("Resenha", systemImage: "text.justify")
+                            ) {
+                                ScrollView(.vertical, showsIndicators: false) {
+                                    Text("Olá, aqui é Maria e você está ouvindo ao podcast Vida de Estudante. Nesse episódio iremos falar sobre os tipos existente de Alfabetos, para utilizar em seus mapas mentais e materiais de estudos.")
+                                        .multilineTextAlignment(.center)
+                                }
                             }
-                            .foregroundColor(.primary)
-                            
-                            Image(systemName: "book.circle.fill")
-                                .resizable()
-                                .padding()
-                                .frame(width: 100, height: 100, alignment: .center)
-                            
-                            Button {
-                                episodeViewModel.createScript(type: 1)
-                            } label: {
-                                Text("Iniciar")
-                            }
-                            .buttonStyle(BlueButton())
-                            .padding(.bottom, 8)
+                            .groupBoxStyle(groupBoxStroked())
+                            .modifier(textFieldPadding())
+                            .buttonStyle(PlainButtonStyle())
+                            .frame(minHeight: 200)
                         }
+                    } else {
+                        GroupBox {
+                            VStack {
+                                HStack {
+                                    Text("Iniciar Teleprompter")
+                                        .fontWeight(.semibold)
+                                        .font(.subheadline)
+                                    Spacer()
+                                    Image(systemName: "chevron.forward")
+                                        .modifier(groupBoxChevron())
+                                }
+                                .foregroundColor(.primary)
+                                
+                                Image(systemName: "book.circle.fill")
+                                    .resizable()
+                                    .padding()
+                                    .frame(width: 100, height: 100, alignment: .center)
+                                
+                                Button {
+                                    episodeViewModel.createScript(type: 1)
+                                } label: {
+                                    Text("Iniciar")
+                                }
+                                .buttonStyle(OrangeButton())
+                                .padding(.bottom, 8)
+                            }
+                            .padding()
+                        }
+                        .groupBoxStyle(groupBoxStroked())
+                        .modifier(textFieldPadding())
                     }
-                    .padding(.horizontal, 25)
-                    .padding(.bottom, 25)
+                    
+                    Text("Lançamento")
+                        .fontWeight(.semibold)
+                        .modifier(textFieldTitle())
+                    
+                    DatePicker("", selection: $actualDate, in: ...Date(), displayedComponents: .date)
+                        .datePickerStyle(.automatic)
+                        .accentColor(Color("accent-color"))
+                        .background(Color.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.black)
+                        )
+                        .frame(width: 20, alignment: .leading)
+                        .offset(x: 15)
+                    
                 }
-                
-                Text("Lançamento")
-                    .font(.title2)
-                    .padding(.horizontal, 25)
-                
-                DatePicker("", selection: $actualDate, in: ...Date(), displayedComponents: .date)
-                    .foregroundColor(Color.red)
-                    .frame(width: 20, alignment: .leading)
-                    .offset(x: 15)
-                
             }
         }
     }
 }
 
-struct BlueButton: ButtonStyle {
+// MARK: - STYLE MODIFIERS
+struct textFieldPadding: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(.horizontal, 40)
+            .padding(.bottom, 25)
+    }
+}
+
+struct textFieldTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 28))
+            .padding(.horizontal, 40)
+            .padding(.bottom, 10)
+    }
+}
+
+struct groupBoxChevron: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 14, weight: .bold))
+            .foregroundColor(Color("accent-color"))
+    }
+}
+
+struct OrangeButton: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding(.horizontal, 70)
             .padding(.vertical, 7)
-            .background(Color("blue-button"))
+            .background(Color("accent-color"))
             .foregroundColor(.white)
-            .cornerRadius(40)
+            .cornerRadius(10)
+            .font(.system(size: 20, weight: .semibold))
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(.black)
+            )
+    }
+}
+
+struct groupBoxStroked: GroupBoxStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack {
+            HStack{
+                configuration.label
+            }
+            configuration.content
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(.black)
+                )
+        }
+        .background(Color.white)
     }
 }
 
